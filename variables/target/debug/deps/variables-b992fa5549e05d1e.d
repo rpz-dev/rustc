c504b4b1c0c0b5c4a950baeb96a8171c3259128c
Rodrigo Pimenta Ramos <57777648+rpz-dev@users.noreply.github.com>
2023-04-28 15:02:28 -0300

    Add files via upload

---
+++ variables/target/debug/deps/variables-b992fa5549e05d1e.d
@@ -0,0 +1,5 @@
+/home/testesi/variables/target/debug/deps/variables-b992fa5549e05d1e: src/main.rs
+
+/home/testesi/variables/target/debug/deps/variables-b992fa5549e05d1e.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/testesi/variables/target/debug/variables: /home/testesi/variables/src/main.rs
